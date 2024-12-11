ce1a9e74d8fe2361fc6c8f859ea146a068372087
Dionovan_Ramadhani <dionovan7@gmail.com>
2024-12-11 10:19:15 +0700

    initiate next js pages router project

---
+++ src/pages/api/hello.ts
@@ -0,0 +1,12 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
+
+type Data = {
+  name: string;
+  age: number;
+  region: string;
+};
+
+export default function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+  res.status(200).json({ name: "Rusdi Ngawi", age: 20, region: "Ngawi" });
+}
