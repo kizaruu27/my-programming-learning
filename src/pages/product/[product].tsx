df95254cd9234e57b01ab06545392e9bc7074a44
Dionovan Ramadhani <80899280+kizaruu27@users.noreply.github.com>
2024-12-15 23:42:54 +0700

    continue learn dynamic routes rendering

--- src/pages/product/[product].tsx
+++ src/pages/product/[product].tsx
@@ -1,21 +1,63 @@
 import { fetchProduct } from "@/lib/axios/productController";
+import { ProductType } from "@/types/product/product.type";
 import DetailProduct from "@/views/detailProduct";
 import { useRouter } from "next/router";
 import useSWR from "swr";
 
-export default function ProductDetailPage() {
+// Server Side Rendering
+// props params dapat mengambil param dari routes halaman
+// export async function getServerSideProps({ params }: { params: { product: string } }) {
+//   const response = await fetchProduct(
+//     `http://localhost:3000/api/product/${params?.product}`
+//   );
+
+//   return {
+//     props: {
+//       product: response?.data,
+//     },
+//   };
+// }
+
+// Static side generation
+export async function getStaticPaths() {
+  const response = await fetchProduct("http://localhost:3000/api/product");
+
+  const paths = response?.data?.map((product: ProductType) => ({
+    params: {
+      product: product?.id,
+    },
+  }));
+
+  return { paths, fallback: false };
+}
+
+// Static side generation
+export async function getStaticProps({ params }: { params: { product: string } }) {
+  const response = await fetchProduct(
+    `http://localhost:3000/api/product/${params?.product}`
+  );
+
+  return {
+    props: {
+      product: response?.data,
+    },
+  };
+}
+
+export default function ProductDetailPage({ product }: { product: ProductType }) {
   // Digunakan untuk mengambil data dari params
   const { query } = useRouter();
-  const { data, isLoading, error } = useSWR(
-    `/api/product/${query.product}`,
-    fetchProduct
-  );
+
+  // Client Side Rendering
+  // const { data, isLoading, error } = useSWR(
+  //   `/api/product/${query.product}`,
+  //   fetchProduct
+  // );
 
   return (
     <div>
-      <h1>Product Page</h1>
       {/* Nama params diambil dari nama file/folder */}
-      <DetailProduct product={data?.data} isLoading={isLoading} />
+      <DetailProduct product={product} />
     </div>
   );
 }
--- src/views/detailProduct/index.tsx
+++ src/views/detailProduct/index.tsx
@@ -1,39 +1,27 @@
 import { ProductType } from "@/types/product/product.type";
-import styles from "../product/product.module.scss";
+import styles from "./detailProduct.module.scss";
 import Link from "next/link";
 
-export default function DetailProduct({
-  product,
-  isLoading,
-}: {
-  product: ProductType;
-  isLoading: boolean;
-}) {
+export default function DetailProduct({ product }: { product: ProductType }) {
   return (
     <>
-      {isLoading ? (
-        <div className={styles.product__content__skeleton}>
-          <div className={styles.product__content__skeleton__image} />
-          <div className={styles.product__content__skeleton__name} />
-          <div className={styles.product__content__skeleton__category} />
-          <div className={styles.product__content__skeleton__price} />
-        </div>
-      ) : (
-        <Link href={`/product/${product.id}`} className={styles.product__content__item}>
+      <h1 className={styles.title}>Product Detail</h1>
+      {product && (
+        <div className={styles.productDetail}>
           <img
             src={product.image}
             alt={product.name}
-            className={styles.product__content__item__image}
+            className={styles.productDetail__image}
           />
-          <h4 className={styles.product__content__item__name}>{product.name}</h4>
-          <p className={styles.product__content__item__category}>{product.category}</p>
-          <p className={styles.product__content__item__price}>
+          <h4 className={styles.productDetail__name}>{product.name}</h4>
+          <p className={styles.productDetail__category}>{product.category}</p>
+          <p className={styles.productDetail__price}>
             {product.price.toLocaleString("id-ID", {
               style: "currency",
               currency: "IDR",
             })}
           </p>
-        </Link>
+        </div>
       )}
     </>
   );
