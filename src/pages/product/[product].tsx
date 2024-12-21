8e8a06b3490a747e0eaa20d2c248bac56b6c65e4
Dionovan Ramadhani <dionovanramadhani@Dionovans-MacBook-Air.local>
2024-12-21 09:52:31 +0700

    change env api URL

--- src/pages/product/[product].tsx
+++ src/pages/product/[product].tsx
@@ -6,7 +6,33 @@ import useSWR from "swr";
 
 // Server Side Rendering
 // props params dapat mengambil param dari routes halaman
-// export async function getServerSideProps({ params }: { params: { product: string } }) {
+export async function getServerSideProps({ params }: { params: { product: string } }) {
+  const response = await fetchProduct(
+    `${process.env.PUBLIC_API_URL}/api/product/${params?.product}`
+  );
+
+  return {
+    props: {
+      product: response?.data,
+    },
+  };
+}
+
+// Static side generation
+// export async function getStaticPaths() {
+//   const response = await fetchProduct("http://localhost:3000/api/product");
+
+//   const paths = response?.data?.map((product: ProductType) => ({
+//     params: {
+//       product: product?.id,
+//     },
+//   }));
+
+//   return { paths, fallback: false };
+// }
+
+// Static side generation
+// export async function getStaticProps({ params }: { params: { product: string } }) {
 //   const response = await fetchProduct(
 //     `http://localhost:3000/api/product/${params?.product}`
 //   );
@@ -17,32 +43,6 @@ import useSWR from "swr";
 //     },
 //   };
 // }
-
-// Static side generation
-export async function getStaticPaths() {
-  const response = await fetchProduct("http://localhost:3000/api/product");
-
-  const paths = response?.data?.map((product: ProductType) => ({
-    params: {
-      product: product?.id,
-    },
-  }));
-
-  return { paths, fallback: false };
-}
-
-// Static side generation
-export async function getStaticProps({ params }: { params: { product: string } }) {
-  const response = await fetchProduct(
-    `http://localhost:3000/api/product/${params?.product}`
-  );
-
-  return {
-    props: {
-      product: response?.data,
-    },
-  };
-}
 
 export default function ProductDetailPage({ product }: { product: ProductType }) {
   // Digunakan untuk mengambil data dari params
--- src/pages/product/server.tsx
+++ src/pages/product/server.tsx
@@ -7,7 +7,7 @@ export async function getServerSideProps() {
   // fetch data
   // const data = await fetch("/api/product");
   // const response = await data?.json();
-  const response = await fetchProduct("http://localhost:3000/api/product");
+  const response = await fetchProduct(`${process.env.PUBLIC_API_URL}/api/product`);
   console.log(response);
 
   // getServerSideProps harus mereturn sebuah object dengan format seperti ini
