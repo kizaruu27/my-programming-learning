e8ba539a09f29e38f61372aca7c8f01e4b51b731
Dionovan Ramadhani <dionovanramadhani@Dionovans-MacBook-Air.local>
2025-01-08 08:40:40 +0700

    learn linking and navigating

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,20 +1,46 @@
 import Link from "next/link";
+import { usePathname, useRouter } from "next/navigation";
 
 export default function Navbar() {
+  const pathname = usePathname();
+  const { push } = useRouter();
+
   return (
-    <nav className="flex bg-gray-800 py-2 px-5">
-      <h1 className="text-white">Navbar</h1>
-      <ul className="flex gap-3 ml-5 text-blue-300">
-        <li className="cursor-pointer">
-          <Link href="/">Home</Link>
-        </li>
-        <li className="cursor-pointer">
-          <Link href="/about">About</Link>
-        </li>
-        <li className="cursor-pointer">
-          <Link href="/about/profile">Profile</Link>
-        </li>
-      </ul>
+    <nav className="flex justify-between bg-gray-800 py-2 px-5">
+      <div className="flex">
+        <h1 className="text-white">Navbar</h1>
+        <ul className="flex gap-3 ml-5">
+          <li
+            className={`cursor-pointer ${
+              pathname === "/" ? "text-blue-300" : "text-white"
+            }`}
+          >
+            <Link href="/">Home</Link>
+          </li>
+          <li
+            className={`cursor-pointer ${
+              pathname === "/about" ? "text-blue-300" : "text-white"
+            }`}
+          >
+            <Link href="/about">About</Link>
+          </li>
+          <li
+            className={`cursor-pointer ${
+              pathname === "/about/profile" ? "text-blue-300" : "text-white"
+            }`}
+          >
+            <Link href="/about/profile">Profile</Link>
+          </li>
+        </ul>
+      </div>
+      <div>
+        <button
+          onClick={() => push("/login")}
+          className="bg-white px-4 text-sm rounded-md h-6"
+        >
+          Login
+        </button>
+      </div>
     </nav>
   );
 }
