d2f22ab789d48b643b0513d25c221b17def07a23
Dionovan Ramadhani <80899280+kizaruu27@users.noreply.github.com>
2025-02-04 20:55:53 +0700

    init next js project

---
+++ app/new-post/page.js
@@ -0,0 +1,37 @@
+import { storePost } from "@/lib/posts";
+
+export default function NewPostPage() {
+  // Jika function akan dipanggil di form action, set function sebagai use server
+  const createPost = async (formdata) => {
+    "use server";
+    const title = formdata.get("title");
+    const image = formdata.get("image");
+    const content = formdata.get("content");
+
+    console.log(title, image, content);
+  };
+
+  return (
+    <>
+      <h1>Create a new post</h1>
+      <form action={createPost}>
+        <p className="form-control">
+          <label htmlFor="title">Title</label>
+          <input type="text" id="title" name="title" />
+        </p>
+        <p className="form-control">
+          <label htmlFor="image">Image URL</label>
+          <input type="file" accept="image/png, image/jpeg" id="image" name="image" />
+        </p>
+        <p className="form-control">
+          <label htmlFor="content">Content</label>
+          <textarea id="content" name="content" rows="5" />
+        </p>
+        <p className="form-actions">
+          <button type="reset">Reset</button>
+          <button>Create Post</button>
+        </p>
+      </form>
+    </>
+  );
+}
